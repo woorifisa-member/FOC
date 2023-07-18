6e9fcb9e0a2ff4dc48f24f7dd8a2375a526deb82
Dongyeol <eastheat10@gmail.com>
2023-07-18 17:41:59 +0900

    first commit

---
+++ server/src/main/java/com/woorifisa/foc/FocApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.foc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocApplication.class, args);
+	}
+
+}
